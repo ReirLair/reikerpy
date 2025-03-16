5df211e7200eb655a215e750595e1c7a92df77db
ReirLair <indiaserverff32@gmail.com>
2025-03-16 10:28:35 +0100

    Create Ignoreiker.py

---
+++ home/Ignoreiker.py
@@ -0,0 +1,12 @@
+# This is just an example file 
+# Python Server by https://t.me/reikertechtools
+# Give Credits As you Can Create as many servers as possible
+
+# E
+#   N
+#     J
+#       O
+#         YOUR FREE USAGE
+
+
+# REIKER 2025
